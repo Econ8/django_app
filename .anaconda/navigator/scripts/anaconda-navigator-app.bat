80ff6bb2177912ee26a5fa724ca137b1dce14dcf
Econ8 <yosuke.kyoto.tokyo@gmail.com>
2020-10-13 21:24:05 +0900

    first commit

---
+++ .anaconda/navigator/scripts/anaconda-navigator-app.bat
@@ -0,0 +1,3 @@
+chcp 932
+call C:\Users\yosuk\Anaconda3\Scripts\activate C:\Users\yosuk\Anaconda3
+anaconda-navigator >C:\Users\yosuk\SampleProject\.anaconda\navigator\scripts\anaconda-navigator-app-out-1.txt 2>C:\Users\yosuk\SampleProject\.anaconda\navigator\scripts\anaconda-navigator-app-err-1.txt
---
+++ .anaconda/navigator/scripts/anaconda-navigator-updater.bat
@@ -0,0 +1,3 @@
+chcp 932
+call C:\Users\yosuk\Anaconda3\Scripts\activate C:\Users\yosuk\Anaconda3
+navigator-updater --latest-version 1.9.12 --prefix C:\Users\yosuk\Anaconda3 >C:\Users\yosuk\SampleProject\.anaconda\navigator\scripts\anaconda-navigator-updater-out-2.txt 2>C:\Users\yosuk\SampleProject\.anaconda\navigator\scripts\anaconda-navigator-updater-err-2.txt
